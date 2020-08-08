24aa5e44d99dcf630ad73350cdac73c7a701e1ed
SAADAT619 <65817171+SAADAT619@users.noreply.github.com>
2020-08-08 11:34:01 +0600

    null done

---
+++ nullUndefined.js
@@ -0,0 +1,18 @@
+// What is undefinied
+let pakhi;
+console.log(pakhi); // It will show undefined cz pakhi variable doesn't contain any values
+
+
+// Another example of undefined
+function add(num1,num2){
+    console.log(num1 + num2);
+}
+const result = add(13,82);
+console.log(result); // It will show undefined cz the function doen't return anything
+
+// Another emample of undefined by object 
+const student = {name:"Saadat", phone:1212323};
+console.log(student.gf);
+// ---------------------------------------------------------------------
+
+// Example of null
