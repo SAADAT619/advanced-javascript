761629e3e615232e2217830dfca9831d916bd5e5
SAADAT619 <65817171+SAADAT619@users.noreply.github.com>
2020-08-08 11:03:28 +0600

    true and false values

---
+++ ifElse.js
@@ -0,0 +1,33 @@
+// Falsy values are:
+// 0
+// undefinied
+// NaN
+// ""
+// null
+// false
+
+// Truethy values are:
+// "0"
+// 12
+// " "
+// [] array
+// {} object
+// "false"
+// true
+// "true"
+
+
+
+
+
+
+
+const age = 12;
+
+if(age){
+    console.log("The condition is true");// 0 bade jekono songkhake javascript true hishebe dhore nibe jodi na if er moddhe kono condition set kora na hoi
+
+}
+else{
+    console.log("The condiotion is false");
+}
